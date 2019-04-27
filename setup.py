ada0719c625fc8c6c4b4121bbbbc622da6625f42
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-04-27 16:20:40 -0600

    dummy setup for local pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='mortgage',
+   version='0.1',
+   description='rent or own calculations',
+   author='Ian Preston',
+   packages=['mortgage'],  #same as name
+)
