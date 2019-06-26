a3bb14725e8ba4fa359b2f8053abf14deb075838
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-06-26 14:42:50 -0600

    docstring cleanup

--- mortgage/asset.py
+++ mortgage/asset.py
@@ -4,8 +4,20 @@ import numpy as np
 
 
 def annual_to_monthly_return(annual_return):
-    """Convert annual return to monthly"""
-    return (1 + annual_return) ** (1 / 12) - 1
+    """Convert annual return to monthly
+    
+    Parameters
+    ----------
+    annual_return: float
+        annualized percentage return
+    
+    Returns
+    -------
+    monthly_return: float
+        monthly percentage return
+    """
+    monthly_return = (1 + annual_return) ** (1 / 12) - 1
+    return monthly_return
 
 
 def annual_to_monthly_stdev(annual_stdev):
@@ -16,8 +28,19 @@ def annual_to_monthly_stdev(annual_stdev):
     would just be the formula below. I haven't looked into what assumptions have to
     hold for this to be valid and I'm planning to be a bit more careful with how I
     parameterise things. But as a first approximation it's a decent helper
+
+    Parameters
+    ----------
+    annual_stdev: float
+        annualized standard deviation
+    
+    Returns
+    -------
+    monthly_stdev: float
+        monthly standard deviation
     """
-    return annual_stdev / math.sqrt(12)
+    monthly_stdev = annual_stdev / math.sqrt(12)
+    return monthly_stdev
 
 
 class BaseAsset:
@@ -37,6 +60,27 @@ class BaseAsset:
     some way of knowing your current and future TFSA and RRSP contribution room and 
     applying appropriate taxes on deposits above and beyond that amount, but it doesn't
     yet.
+
+    Parameters
+    ----------
+    dist: numpy distribution, default np.random.normal
+        The distribution from which returns are drawn
+    dist_args: dict, default mean 0.006, standard deviation 0.06
+        dictionary of kwargs to be passed along with dist
+    periods: int, default 300
+        Number of periods to simulate
+    simulations: int, default 100
+        Number of simulations to run
+    
+    Returns
+    -------
+    returns: numpy array
+        periods x simulations array of cumulative returns for the asset
+        I think of it as the price of a security (with no dividends etc)
+        normalized to = 1 in the first period. You can then take a series of
+        cash flows to compute the number of shares in that asset you can buy
+        in a given period, cumulatively sum those shares, and then multiply them by
+        the asset price for any period to determine total wealth accumulated
     """
 
     def __init__(
