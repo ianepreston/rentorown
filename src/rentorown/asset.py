80d693bb3ee0aa030c679481a25da573320021f7
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-04-02 14:17:26 +0000

    docstring cleanup

--- src/rentorown/asset.py
+++ src/rentorown/asset.py
@@ -1,27 +1,27 @@
-"""Basic asset class to escalate financial flows"""
+"""Basic asset class to escalate financial flows."""
 import math
+from typing import Dict, Callable, Optional
 import numpy as np
 
 
-def annual_to_monthly_return(annual_return):
-    """Convert annual return to monthly
-    
+def annual_to_monthly_return(annual_return: float) -> float:
+    """Convert annual return to monthly.
+
     Parameters
     ----------
     annual_return: float
         annualized percentage return
-    
+
     Returns
     -------
-    monthly_return: float
+    float
         monthly percentage return
     """
-    monthly_return = (1 + annual_return) ** (1 / 12) - 1
-    return monthly_return
+    return (1 + annual_return) ** (1 / 12) - 1
 
 
-def annual_to_monthly_stdev(annual_stdev):
-    """Be careful with this one
+def annual_to_monthly_stdev(annual_stdev: float) -> float:
+    """Convert annual standard deviation to monthly. Careful with this one.
 
     It seems like a common method in financial modelling to just take the monthly
     standard deviation and multiply it by sqrt(12) to annualize it. So in reverse that
@@ -33,48 +33,50 @@ def annual_to_monthly_stdev(annual_stdev):
     ----------
     annual_stdev: float
         annualized standard deviation
-    
+
     Returns
     -------
-    monthly_stdev: float
+    float
         monthly standard deviation
     """
-    monthly_stdev = annual_stdev / math.sqrt(12)
-    return monthly_stdev
+    return annual_stdev / math.sqrt(12)
 
 
-class BaseAsset:
-    """Basic way of defining an asset
+def distreturns(
+    dist: Callable = np.random.normal,
+    dist_args: Optional[Dict] = None,
+    periods: int = 300,
+    simulations: int = 100,
+):
+    """Simulate a series of returns from a given distribution.
 
     Lets you parameterize the distribution it's drawn from,
     Specify the number of simulations and periods to simulate for,
     and plot results of the simulations
 
-    I thought this was going to end up a lot more complicated. It could have
-    just been a function.
-
-    TODO
-    ----
+    Notes
+    -----
     Right now this does not assume any tax on capital gains on the portfolio. You can
     hack around that by inputing a tax adjusted expected return. Ideally it would have
-    some way of knowing your current and future TFSA and RRSP contribution room and 
+    some way of knowing your current and future TFSA and RRSP contribution room and
     applying appropriate taxes on deposits above and beyond that amount, but it doesn't
     yet.
 
     Parameters
     ----------
-    dist: numpy distribution, default np.random.normal
+    dist: Callable, default np.random.normal
         The distribution from which returns are drawn
-    dist_args: dict, default mean 0.006, standard deviation 0.06
-        dictionary of kwargs to be passed along with dist
+    dist_args : dict
+        dictionary of kwargs to be passed along with dist defaults to
+        mean 0.006, standard deviation 0.06
     periods: int, default 300
         Number of periods to simulate
     simulations: int, default 100
         Number of simulations to run
-    
+
     Returns
     -------
-    returns: numpy array
+    np.ndarray
         periods x simulations array of cumulative returns for the asset
         I think of it as the price of a security (with no dividends etc)
         normalized to = 1 in the first period. You can then take a series of
@@ -82,16 +84,8 @@ class BaseAsset:
         in a given period, cumulatively sum those shares, and then multiply them by
         the asset price for any period to determine total wealth accumulated
     """
-
-    def __init__(
-        self,
-        dist=np.random.normal,
-        dist_args={"loc": 0.006, "scale": 0.06},
-        periods=300,
-        simulations=100,
-    ):
-        self.returns = (1 + dist(**dist_args, size=(periods, simulations))).cumprod(
-            axis=0
-        )
-        self.returns[0] = 1
-
+    if dist_args is None:
+        dist_args = {"loc": 0.006, "scale": 0.06}
+    returns = (1 + dist(**dist_args, size=(periods, simulations))).cumprod(axis=0)
+    returns[0] = 1
+    return returns
